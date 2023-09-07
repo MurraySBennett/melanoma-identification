95a5fc447aeb538fbc549132d8973c5cf550a98e
Murray Bennett <murray.bennett@uon.edu.au>
2023-09-06 20:55:59 -0500

    merge cv and btl data

--- feature-rating/scripts/applyBTL.py
+++ feature-rating/scripts/applyBTL.py
@@ -7,24 +7,24 @@ from btl_funcs import regression_format, lm
 def main(feature, save_data):
     home_path = "/mnt/c/Users/qlm573/melanoma-identification/"
     data_path = path.join(home_path, "feature-rating", "btl-feature-data")
- 
-    if feature == "symmetry":
-        data = pd.read_csv(path.join(data_path, 'btl-asymmetry.csv'))
-    elif feature == "border":
-        data = pd.read_csv(path.join(data_path, 'btl-border.csv'))
-    elif feature == "colour":
-        data = pd.read_csv(path.join(data_path, 'btl-colour.csv'))
-    else:
-        print("{feature} is invalid input. Please use 'symmetry', 'border', or 'colour'.")
-    ## logistic regression to solve for BTL
-    X, y = regression_format(data)
-    # q, q_mid, q_slope = lm(X, y, penalty='l1')
-    #q = q.to_frame().reset_index().rename(columns={'index': 'id', 0: 'q'})
-    r, r_mid, r_slope = lm(X, y, penalty='l2')
-    r = r.to_frame().reset_index().rename(columns={'index': 'id', 0: 'r'})
-    #ability = pd.merge(q, r, on='id', how='left')
-    if save_data:
-        r.to_csv(path.join(data_path, 'btl-scores-'+feature+'.csv'), index=False)
+    for f in feature: 
+        if f == "symmetry":
+            data = pd.read_csv(path.join(data_path, 'btl-asymmetry.csv'))
+        elif f == "border":
+            data = pd.read_csv(path.join(data_path, 'btl-border.csv'))
+        elif f == "colour":
+            data = pd.read_csv(path.join(data_path, 'btl-colour.csv'))
+        # else:
+        #     print("{f} is invalid input. Please use 'symmetry', 'border', or 'colour'.")
+        ## logistic regression to solve for BTL
+        X, y = regression_format(data)
+        # q, q_mid, q_slope = lm(X, y, penalty='l1')
+        #q = q.to_frame().reset_index().rename(columns={'index': 'id', 0: 'q'})
+        r, r_mid, r_slope = lm(X, y, penalty='l2')
+        r = r.to_frame().reset_index().rename(columns={'index': 'id', 0: 'r'})
+        #ability = pd.merge(q, r, on='id', how='left')
+        if save_data:
+            r.to_csv(path.join(data_path, 'btl-scores-' + f + '.csv'), index=False)
 
 
         
@@ -34,6 +34,6 @@ if __name__ == "__main__":
     # parser.add_argument("feature", choices=["symmetry", "border", "colour"], help="Feature to process")
     # args = parser.parse_args()
     # main(args.feature)
-    feature = 'symmetry'
+    feature = ["symmetry", "border", "colour"]
     save_data = True
     main(feature, save_data)
---
+++ feature-rating/scripts/merge_btl_cv.py
@@ -0,0 +1,23 @@
+from os import path
+import pandas as pd
+
+save_data = True
+home_path = path.join(path.expanduser('~'), 'win_home', 'melanoma-identification', 'feature-rating')
+paths = dict(cv_data=path.join(home_path, 'cv-data'),
+        btl_data=path.join(home_path, 'btl-feature-data')
+        )
+
+btl_sym = pd.read_csv(path.join(paths['btl_data'],'btl-scores-symmetry.csv'), delimiter=',', header=0)
+btl_bor = pd.read_csv(path.join(paths['btl_data'],'btl-scores-border.csv'), delimiter=',', header=0)
+btl_col = pd.read_csv(path.join(paths['btl_data'],'btl-scores-colour.csv'), delimiter=',', header=0)
+
+cv_data = pd.read_csv(path.join(paths['cv_data'], 'cv-data.csv'), delimiter=',', header=0)
+
+# outer merge while-ever you have incomplete data.
+data = cv_data\
+        .merge(btl_sym, on='id', how='outer')\
+        .merge(btl_bor, on='id', how='outer')\
+        .merge(btl_col, on='id', how='outer')
+
+if save_data:
+    data.to_csv(path.join(paths['btl_data'],'btl-cv-data.csv'), index=False)
